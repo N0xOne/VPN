8f931eda4162818dc34993c793c6f3b9d3bc5807
n0x4z3r <n0x4z3r@nx-dvm.mercure.nox-corp.net>
2024-07-07 15:28:57 +0200

    Changement par des regles plus simples, avec strategie plus restrictive

--- firewall.sh
+++ firewall.sh
@@ -4,38 +4,40 @@ OVPN_PROTO="udp"
 IF_TUN="tun0"
 IF_LAN="enp0s8"
 IF_GW_TUN="10.8.0.1"
-IF_GW_TUN="192.168.1.254"
+IF_GW_LAN="192.168.1.254"
 NET_TUN="10.8.0.0/24"
 NET_LAN="192.168.1.0/24"
+IP_LAN="192.168.1.3"
 ALLOW_PORT="80,443"
 
+## Changement de strategie, je passe sur une approche restrictive.
+## Les redirections sont donc rejetes par defaut.
 vim -O /etc/ufw/before.rules INFO/BEFORE_UFW
 vim -O /etc/default/ufw INFO/DEFAULT_UFW
 
+# On autorise les connexion au serveur VPN
+ufw allow in on enp0s8 proto $OVPN_PROTO to $IP_LAN port $OVPN_PORT
+# On autorise les connexion au serveur SSH depuis la LAN
+ufw allow in on enp0s8 proto tcp from 192.168.1.0/24 port 22
+# On isole l'interface du VPN
+ufw deny out on $IF_TUN
+ufw deny in on $IF_TUN
 
-ufw deny in on $IF_TUN to any comment "isolate vpn gateway from others network"
-ufw deny out on $IF_TUN to any comment "isolate vpn gateway from others network"
-ufw allow in on $IF_TUN to any port 80 comment "Allow HTTP"
-ufw allow in on $IF_TUN to any port 443 comment "Allow HTTP(s)"
-ufw allow out on $IF_TUN to any port 80 comment "Allow HTTP"
-ufw allow out on $IF_TUN to any port 443 comment "Allow HTTP(s)"
-ufw deny from $NET_LAN to $NET_TUN comment "isolate lan from vpn network"
+# on ouvre les ports 80,443 (I/O)
+ufw allow in on $IF_TUN proto tcp to any port $ALLOW_PORT
+ufw allow out on $IF_TUN proto tcp to any port $ALLOW_PORT
 
-ufw route deny in on $IF_LAN out on $IF_TUN comment "Disable routing from boths IF"
-ufw route deny in on $IF_TUN out on $IF_LAN comment "Disable routing from boths IF"
+# on active la redirection du vpn vers l'interface LAN
+ufw route allow in on $IF_TUN out on $IF_LAN proto tcp to any port $ALLOW_PORT
+ufw route allow in on $IF_LAN out on $IF_TUN proto tcp to any port $ALLOW_PORT
 
-ufw route allow in on $IF_TUN out on $IF_LAN to $IF_GW_LAN port 80 comment "Allow HTTP forwarding"
-ufw route allow in on $IF_TUN out on $IF_LAN to $IF_GW_LAN port 443 comment "Allow HTTP(s) forwarding"
-ufw route allow in on $IF_LAN out on $IF_TUN from $IF_GW_LAN port 80 comment "Allow HTTP forwarding from WAN"
-ufw route allow in on $IF_LAN out on $IF_TUN from $IF_GW_LAN port 443 comment "Allow HTTP(s) forwarding from WAN"
+# on bloque l'access au reseau VPN depuis la LAN
+ufw deny in from $NET_LAN proto tcp port $ALLOW_PORT
+# Et inversement
+ufw deny out to $NET_LAN proto tcp port $ALLOW_PORT
 
-#ufw allow in on $IF_LAN proto $OVPN_PROTO to any port $OVPN_PORT comment "Accept OpenVPN port"
-#ufw deny in on $IF_LAN proto $OVPN_PROTO to any port $OVPN_PORT from $NET_LAN comment "Deny OpenVPN from lan"
-#ufw deny in on $IF_TUN from $NET_TUN comment "Deny all traffic inside TUN"
-#ufw deny in on $IF_TUN to $NET_TUN comment "Deny all traffic inside TUN"
-#ufw allow in on $IF_TUN to any proto tcp port $ALLOW_PORT from $NET_TUN comment "Allow HTTP(S) traffic inside TUN"
-#ufw route allow in on enp0s8 out on tun0 to $NET_TUN port $ALLOW_PORT from $NET_LAN comment "OpenVPN Forwarding from LAN"
-#ufw route allow in on $IF_TUN out on $IF_LAN to any proto tcp port $ALLOW_PORT from $NET_TUN comment "OpenVPN Forwarding from TUN"
+# on autorise malgres tout l'access a la WAN
+ufw allow out to $IF_GW_LAN proto tcp port $ALLOW_PORT
 
 echo "Redemarrage du pare-feu..."
 ufw disable
